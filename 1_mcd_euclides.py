e999f3105083c87893facec3a4f94455ca2f8df7
Francesc <fxbullich@gmail.com>
2019-09-25 21:38:31 +0200

    [FEAT] MCD euclides + grafic

---
+++ 1_mcd_euclides.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import timeit
+import random
+
+
+def mcd_euclides(dividend , divisor):
+#Calcula el màxim comú divisor de a i b usant l'algorisme d'Euclides
+    residu = dividend % divisor
+    while residu != 0:
+        dividend = divisor
+        divisor = residu
+        residu = dividend % divisor
+
+    return divisor
+
+
+def mcd_input():
+    dividend = input("entreu un dividend natural positiu: ")
+    divisor = input("entreu un divisor natural positiu: ")
+    temps_inicial = timeit.default_timer()
+    resultat = mcd_euclides(int(dividend), int(divisor))
+    temps_final = timeit.default_timer()
+    temps_invertit = temps_final - temps_inicial
+    print("Dividend: ",dividend)
+    print("Divisor: ", divisor)
+    print("MCD: ", resultat)
+    print(len(dividend))
+    print("Temps invertit: %.10f segons" % (temps_invertit*100000))
+
+
+def mcd_rep():
+
+    i = 0
+    llargada = 32
+    llargada2 = 24
+    mides = list()
+    temps = list()
+    while i < 10:
+        dividend = random.getrandbits(llargada)
+        divisor = random.getrandbits(llargada2)
+        llargada *=2
+        llargada2 *=2
+        i +=1
+        temps_inicial = timeit.default_timer()
+        resultat = mcd_euclides(dividend, divisor)
+        temps_final = timeit.default_timer()
+        temps_invertit = temps_final - temps_inicial
+        mides.append(llargada)
+        temps.append(temps_invertit*100000)
+
+
+    plt.plot(temps, mides, '-gD')
+    plt.show()
+
+
+mcd_rep()
